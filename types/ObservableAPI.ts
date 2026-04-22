f893b43eaf53c96dbe86fc8880cd56fdfe096d38
cosmeowpawlitan <50871412+cosmeowpawlitan@users.noreply.github.com>
2026-04-22 12:37:13 -0400

    init

---
+++ types/ObservableAPI.ts
@@ -0,0 +1,23 @@
+import { ResponseContext, RequestContext, HttpFile, HttpInfo } from '../http/http';
+import { Configuration, ConfigurationOptions, mergeConfiguration } from '../configuration'
+import type { Middleware } from '../middleware';
+import { Observable, of, from } from '../rxjsStub';
+import {mergeMap, map} from  '../rxjsStub';
+import { BookkeepingInner } from '../models/BookkeepingInner';
+import { Exercise } from '../models/Exercise';
+import { Expenditure } from '../models/Expenditure';
+import { ExpenditureSplit } from '../models/ExpenditureSplit';
+import { FillInTheBlank } from '../models/FillInTheBlank';
+import { MCQuestion } from '../models/MCQuestion';
+import { OpeningBalance } from '../models/OpeningBalance';
+import { Option } from '../models/Option';
+import { Point } from '../models/Point';
+import { PointReason } from '../models/PointReason';
+import { Question } from '../models/Question';
+import { QuestionsInner } from '../models/QuestionsInner';
+import { Refund } from '../models/Refund';
+import { Requisition } from '../models/Requisition';
+import { RequisitionStatus } from '../models/RequisitionStatus';
+import { TrueFalseNotGivenEnum } from '../models/TrueFalseNotGivenEnum';
+import { TrueFalseNotGivenQuestion } from '../models/TrueFalseNotGivenQuestion';
+import { Usability } from '../models/Usability';
